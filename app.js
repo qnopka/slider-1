6acd05d383202b3fe65d365e9934b90730003c0b
Попович Екатерина <71285738+qnopka@users.noreply.github.com>
2021-06-25 10:33:44 +0300

    upd

--- app.js
+++ app.js
@@ -36,3 +36,16 @@ function changeSlide(direction) {
     sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
 }
 
+setInterval(function () {
+        activeSlideIndex++
+    if(activeSlideIndex === slidesCount) {
+        activeSlideIndex = 0;
+    } else if(activeSlideIndex < 0) {
+        activeSlideIndex = slidesCount - 1;
+    }
+    const height = container.clientHeight;
+
+    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
+    sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
+}, 3000)
+
